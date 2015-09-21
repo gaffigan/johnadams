17bc790d0c04ed23522005b26153e2ad85e9560f
Elwood Berry <me@elwoodberry.com>
2015-09-20 22:12:08 -0400

    updated the mobile footer

--- public/js/demo-only.js
+++ public/js/demo-only.js
@@ -1,11 +1,1 @@
-$(document).ready(function(e) {
-
-	alert("active");
-
-	// MOBILE FOOTER
-	// border-top: 1px solid #2b8ebb;
-	$('.active').click(function(){
-		alert("Clicked");
-	});
-
-});
+$(document).ready(function(e) {});
